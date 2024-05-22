ce78e5ac6b44eeebfa8735df236b73aaaabce0e4
Doru <dorufloare@yahoo.com>
2024-05-22 19:18:48 +0300

    finished the ai

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import math
 import random
 import sys
 import os
+import time
 
 import neat
+import neat.config
 import pygame
+
+#Map (from 1 to 5, increasing in difficulty)
+
+MAP = 'map2.png'
 
 #Constants
 
@@ -21,6 +27,9 @@ CAR_SIZE_Y = 50
 
 DEFAULT_SPEED = 20
 MIN_SPEED = 12
+SPEED_CHANGE = 2
+
+TURN_ANGLE = 10
 
 #The color the cars should not touch
 
@@ -28,6 +37,8 @@ BAD_COLOR = (255, 255, 255, 255)
 SENSOR_COLOR = (0, 255, 0)
 
 MAX_SENSOR_LENGTH = 300
+
+current_generation = 0
 
 def distance2d(x1, y1, x2, y2):
 	delta_x = x1 - x2
@@ -50,7 +61,6 @@ class Car:
 
 		#we store the coordonates and the distance from the border of the sensors
 		self.sensors = []
-		self.drawing_sensors = []
 
 		self.alive = True
 
@@ -59,7 +69,7 @@ class Car:
 		self.time = 0
 
 	def update_center(self):
-		self.center = [self.position[0] + CAR_SIZE_X / 2, self.position[1] + CAR_SIZE_Y / 2]
+		self.center = [int(self.position[0]) + CAR_SIZE_X / 2, int(self.position[1]) + CAR_SIZE_Y / 2]
 
 	def draw(self, screen):
 		screen.blit(self.rotated_sprite, self.position)
@@ -82,26 +92,16 @@ class Car:
 
 	def update_sensor(self, sensor_angle, game_map):
 		length = 0
-		center_x = self.center[0]
-		center_y = self.center[1]
-		total_angle = math.radians(sensor_angle + self.angle)
-		
-		x = 0
-		y = 0
 
-		def update_coords():
-			nonlocal x
-			nonlocal y
-			x = int(center_x + math.cos(total_angle) * length)
-			y = int(center_y + math.sin(total_angle) * length)
-
-		update_coords()
+		x = int(self.center[0] + math.cos(math.radians(360 - (sensor_angle + self.angle))) * length)
+		y = int(self.center[1] + math.sin(math.radians(360 - (sensor_angle + self.angle))) * length)
 
 		while game_map.get_at((x, y)) != BAD_COLOR and length < MAX_SENSOR_LENGTH:
 			length += 1
-			update_coords()
+			x = int(self.center[0] + math.cos(math.radians(360 - (sensor_angle + self.angle))) * length)
+			y = int(self.center[1] + math.sin(math.radians(360 - (sensor_angle + self.angle))) * length)
 
-		dist = distance2d(x, y, center_x, center_y)
+		dist = distance2d(x, y, self.center[0], self.center[1])
 
 		self.sensors.append([(x, y), dist])
 
@@ -130,23 +130,21 @@ class Car:
 			self.speed_set = True
 
 		self.rotated_sprite = self.rotate_center(self.sprite, self.angle)
-		self.position[0] += math.cos(math.radians(self.angle)) * self.speed
-		self.position[1] += math.sin(math.radians(self.angle)) * self.speed
-		self.correct_positions()
-		self.update_center()
+		self.position[0] += math.cos(math.radians(360 - self.angle)) * self.speed
+		self.position[1] += math.sin(math.radians(360 - self.angle)) * self.speed
+		#self.correct_positions()
+		self.update_center()	
 
 		self.distance += self.speed
 		self.time += 1
 
 		#calculate the 4 corners
 		length = 0.5 * CAR_SIZE_X
-		center_x = self.center[0]
-		center_y = self.center[1]
 
-		left_top 			= [center_x + math.cos(math.radians((self.angle + 30)))  * length, center_y + math.sin(math.radians((self.angle + 30)))  * length]
-		right_top 		= [center_x + math.cos(math.radians((self.angle + 150))) * length, center_y + math.sin(math.radians((self.angle + 150))) * length]
-		left_bottom 	= [center_x + math.cos(math.radians((self.angle + 210))) * length, center_y + math.sin(math.radians((self.angle + 210))) * length]
-		right_bottom	= [center_x + math.cos(math.radians((self.angle + 330))) * length, center_y + math.sin(math.radians((self.angle + 330))) * length]
+		left_top 			= [self.center[0] + math.cos(math.radians(360 - (self.angle + 30)))  * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 30)))  * length]
+		right_top 		= [self.center[0] + math.cos(math.radians(360 - (self.angle + 150))) * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 150))) * length]
+		left_bottom 	= [self.center[0] + math.cos(math.radians(360 - (self.angle + 210))) * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 210))) * length]
+		right_bottom	= [self.center[0] + math.cos(math.radians(360 - (self.angle + 330))) * length, self.center[1] + math.sin(math.radians(360 - (self.angle + 330))) * length]
 		self.corners 	= [left_top, right_top, left_bottom, right_bottom]
 
 		self.check_collision(game_map)
@@ -168,37 +166,109 @@ class Car:
 		return sensor_data
 
 
-def run_simulation():
+def run_simulation(genomes, config):
+	networks = []
+	cars = []
+
 	pygame.init()
-	screen = pygame.display.set_mode((WIDTH, HEIGHT))
-	game_map = pygame.image.load('map.png').convert()
-	car = Car()
+	screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 
-	clock = pygame.time.Clock() 
+	for i, genome in genomes:
+		net = neat.nn.FeedForwardNetwork.create(genome, config)
+		networks.append(net)
+		genome.fitness = 0
+		cars.append(Car())
 
-	running = True
-	while running:
+	clock = pygame.time.Clock()
+	generation_font = pygame.font.SysFont("Arial", 30)
+	alive_font = pygame.font.SysFont("Arial", 20)
+	game_map = pygame.image.load(MAP).convert()
+
+	global current_generation
+	current_generation += 1
+
+	start_timer = time.time()
+
+	while True:
+		skip_generation = False
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				running = False
+				sys.exit(0)
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_ESCAPE:
-					running = False
-
-		screen.blit(game_map, (0, 0))
-
-		#print(car.position)
+					sys.exit(0)
+				elif event.key == pygame.K_SPACE:
+					skip_generation = True
+					break
 		
-		car.update_sensors(game_map)
-		car.draw(screen)
-		car.update(game_map)
+		if skip_generation:
+			break
 
-		pygame.display.flip() 
-	
+		for i, car in enumerate(cars):
+			#add our sensor data into the network
+			output = networks[i].activate(car.get_data())
+
+			#choose the best choice
+			# 0 = left
+			# 1 = right
+			# 2 - speed up
+			# 3 - slow down
+			choice = output.index(max(output))
+
+			if choice == 0:
+				car.angle += TURN_ANGLE
+			elif choice == 1:
+				car.angle -= TURN_ANGLE
+			elif choice == 2:
+				car.speed += SPEED_CHANGE
+			elif car.speed - SPEED_CHANGE >= MIN_SPEED:
+				car.speed -= SPEED_CHANGE
+
+		cars_alive = 0
+		for i, car in enumerate(cars):
+			if car.is_alive():
+				cars_alive += 1
+				car.update(game_map)
+				genomes[i][1].fitness += car.get_reward()
+
+		#all cars are dead or too much time elapsed
+		if cars_alive == 0 or time.time() - start_timer > 25:
+			break
+		
+		screen.blit(game_map, (0, 0))
+		for car in cars:
+			if car.is_alive():
+				car.draw(screen)
+
+		#Generation and car counter texts
+
+		text = generation_font.render("Generation: " + str(current_generation), True, (0,0,0))
+		text_rect = text.get_rect()
+		text_rect.center = (900, 450)
+		screen.blit(text, text_rect)
+
+		text = alive_font.render("Still Alive: " + str(cars_alive), True, (0, 0, 0))
+		text_rect = text.get_rect()
+		text_rect.center = (900, 490)
+		screen.blit(text, text_rect)
+
+		pygame.display.flip()  
 		clock.tick(60)  # Run at 60 frames per second
 	
 	pygame.quit() 
 
 if __name__ == "__main__":
-	run_simulation()
+	config_path = "./config.txt"
+	config = neat.config.Config(neat.DefaultGenome,
+														  neat.DefaultReproduction,
+														  neat.DefaultSpeciesSet,
+														  neat.DefaultStagnation,
+														  config_path)
+	
+	population = neat.Population(config)
+	population.add_reporter(neat.StdOutReporter(True))
+	stats = neat.StatisticsReporter()
+	population.add_reporter(stats)
+	
+	population.run(run_simulation, 1000)
 
